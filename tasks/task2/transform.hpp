8d6cfe5bc4da117315cb63f2e1f40e8c1e079027
Айнур <fattahovainur0gmailcom>
2023-06-15 09:59:57 +0500

    Update task_2 solution.

--- tasks/task2/transform.hpp
+++ tasks/task2/transform.hpp
@@ -3,23 +3,17 @@ void TransformIf(T* begin, T* end, bool (*p)(const T&), void (*f)(T&)) {
     std::vector<T> copyP;
     std::vector<uint64_t> iP;    
     for (T* it = begin; it <= end; ++it) {
-        bool isP = false;
-        try {
-            if (p(*it))
-                isP = true;
-        } catch (...) {}
         bool isCopyThrows = false;
         try {
-            if (isP) {
-                copyP.push_back(*it);
-                iP.push_back(it - begin);
-            }
-        } catch (...) {
-            isCopyThrows = true;
-        }
-        try {
-            if (p(*it))
+            if (p(*it)) {
+            	try {
+            		copyP.push_back(*it);
+                	iP.push_back(it - begin);
+            	} catch (...) {
+            		isCopyThrows = true;
+            	}
                 f(*it);
+            }
         } catch (...) {
             if (isCopyThrows)
                 throw;
